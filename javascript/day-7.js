876ecd9253808aa9fc6554e8a7b5bd2c7381eab4
[200~anshulojha1214@gmail.com <anshulojha1214@gmail.com>
2025-09-22 19:34:23 +0530

    into to js

--- javascript/day-7.js
+++ javascript/day-7.js
@@ -32,50 +32,77 @@
 //objects
 
 
-let a;
-console.log(a);
+// let a;
+// console.log(a);
 
-let b=null;
-console.log(b);
+// let b=null;
+// console.log(b);
 
 // arrays
-let set=[1,2,4,"check",false];
+// let set=[1,2,4,"check",false];
 
 // objects
-let person={
-    name:"Anshul",
-    age:27,
-    marks:87
-}
+// let person={
+//     name:"Anshul",
+//     age:27,
+//     marks:87
+// }
 
 // operators
 
 // Arithmetic operators
-let d=30;
-let s=20;
+// let d=30;
+// let s=20;
 
-console.log(d+s);
-console.log(d-s);
-console.log(d*s);
-console.log(d/s);
-console.log(d%s);
+// console.log(d+s);
+// console.log(d-s);
+// console.log(d*s);
+// console.log(d/s);
+// console.log(d%s);
 
 
 // Assignment operators
 
-let x=10;
+// let x=10;
 
 // Short-hand assignment operators
-x=x-2;
+// x=x-2;
 
-x -=2;
+// x -=2;
 
-x=x+2;
-x+=2;
+// x=x+2;
+// x+=2;
 
-x=x*2;
-x*=2;
+// x=x*2;
+// x*=2;
 
-x=x/2;
-x/=2;
+// x=x/2;
+// x/=2;
+
+// comparison operators
+
+let a=10;
+let b=20;
+
+// equality
+// console.log(a==b);
+
+let c="10";
+
+// loose checking
+console.log(a==c);
+
+// Strict checking
+console.log(a===c);
+
+console.log(a!==b);
+
+console.log(a<b);
+console.log(a<=b);
+console.log(a>b);
+console.log(a>=b);
+
+let e=20;
+let f=40;
+
 
