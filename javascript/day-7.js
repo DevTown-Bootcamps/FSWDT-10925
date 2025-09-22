b8554c4f8bcfed8710d44660511b4c187cae0d93
[200~anshulojha1214@gmail.com <anshulojha1214@gmail.com>
2025-09-22 19:23:34 +0530

    into to js

---
+++ javascript/day-7.js
@@ -0,0 +1,81 @@
+// Variable declaration
+
+// scoped variable and un-scoped variable
+
+//let is blocked scoped variable.
+//the value can be changed in future.
+// {
+// let a="String";
+// console.log(a);
+// }
+
+//it is not a scoped variable-you can access it outside of the scope.
+//you can also change the value later.
+// {
+// var b=12;
+// }
+// console.log(b);
+
+// constant-if a value is given to it then it cannot be changed in the future.
+//const is a scoped variable
+// {
+// const c=20;
+// console.log(c);
+// }
+
+//Number-10,-1,10.2
+//boolean-true,false
+//Strings
+//undfined
+//null
+//arrays
+//objects
+
+
+let a;
+console.log(a);
+
+let b=null;
+console.log(b);
+
+// arrays
+let set=[1,2,4,"check",false];
+
+// objects
+let person={
+    name:"Anshul",
+    age:27,
+    marks:87
+}
+
+// operators
+
+// Arithmetic operators
+let d=30;
+let s=20;
+
+console.log(d+s);
+console.log(d-s);
+console.log(d*s);
+console.log(d/s);
+console.log(d%s);
+
+
+// Assignment operators
+
+let x=10;
+
+// Short-hand assignment operators
+x=x-2;
+
+x -=2;
+
+x=x+2;
+x+=2;
+
+x=x*2;
+x*=2;
+
+x=x/2;
+x/=2;
+
