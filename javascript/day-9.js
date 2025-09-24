ca45c85a908f3933e13a9a0ebe48af0a6a4910b7
[200~anshulojha1214@gmail.com <anshulojha1214@gmail.com>
2025-09-24 19:37:03 +0530

    loops

---
+++ javascript/day-9.js
@@ -0,0 +1,159 @@
+//Swith-case
+//A-EXCELLENT
+//B-good job
+//C-good
+//D-Word hard
+//E-Just doing
+//F-fail
+
+// let grade='A';
+
+// switch(grade){
+//   case 'A':
+//     console.log();
+//     break;
+// }
+
+//loops in js
+
+//for loop
+
+// for(intialization,condition,updation)
+
+
+// Loop in js
+
+// For loop
+
+// for(intialization,condition,updation)
+// a++; ++a; incremental statement a=a=+1;
+//i=0; true; execution of statement ; i=1
+//i=1; true; execute; i=2;
+//i=2; true; execute; i=3;
+//i=3; true; execute; i=4;
+//i=4; true; execute; i=5;
+//i=5;true;execute; i=6;
+// i=6; false; end the loop
+
+
+// for(let i=0;i<=5;i++){
+//     console.log(i);
+// }
+
+// let fruit=["apple","bannana","cherry"];
+
+// for(let i=0;i<fruit.length;i++){
+//      console.log(fruit[i]);
+// }
+
+// let nums=[1,2,3,4,5,6];
+
+// for(let j=0;j<nums.length;j++){
+//     nums[j]=nums[j]+2; 
+//     //nums[j]+=2;
+//     console.log(nums[j]);
+// }
+
+// break;
+// range=1-100
+// print all numbers till you reach first prime number;
+
+// for(let i=0;i<=5;i++){
+//    console.log(i);
+//    if(i==3){
+//     break;
+//    }
+// }
+
+// for(let i=0;i<=5;i++){
+//    if(i==4){
+//     continue;
+//    }
+//    console.log(i);
+// }
+
+//create a array of number from 1 to 10, 
+// and update each element by nums[i]=nums[i]*2; untill the index of array is 7;
+
+// Nested loop-5*5=25; n*m times;
+// let a=1;
+// for(let i=0;i<5;i++){
+//     for(let j=0;j<5;j++){
+//       console.log(a++);
+//     }
+// }
+// i=0:
+//  j:0
+//  j:1
+//  j:2
+//  j:3
+//  j:4
+// i=1:
+//  j:0
+//  j:1
+//  j:2
+//  j:3
+//  j:4
+// i=2:
+//  j:0
+//  j:1
+//  j:2
+//  j:3
+//  j:4
+// i=3:
+//  j:0
+//  j:1
+//  j:2
+//  j:3
+//  j:4
+// i=4:
+//  j:0
+//  j:1
+//  j:2
+//  j:3
+//  j:4
+
+
+
+
+// while loop
+// let i=0;
+// while(i<5){
+//   console.log(i);
+//   i++;
+// }
+
+// while(i<=5){
+//     if(i==4){
+//         break;
+//     }
+//     console.log(i);
+//     i++;
+// }
+
+//create a array of number from 1 to 10, 
+// and update each element by nums[i]=nums[i]*2; untill the index of array is 7;
+
+// do-while
+
+let j=4;
+
+do{
+    console.log(j);
+    j++;
+}while(j<4)
+
+// let nums=[1,2,3,4,5,6,7,8,9,10];
+// let i=0;
+// do{
+//     if(i==7){
+//         break;
+//     }
+//     nums[i]=nums[i]*2;
+//     console.log(nums[i]);
+//     i++;
+// }while(i<nums.length)
+
+
+
+
