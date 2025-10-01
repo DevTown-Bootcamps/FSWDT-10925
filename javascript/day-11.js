c891ec4a2fff55d9dd51d22ece3d094b6a9ef7a8
[200~anshulojha1214@gmail.com <anshulojha1214@gmail.com>
2025-10-01 19:15:14 +0530

    arrays

---
+++ javascript/day-11.js
@@ -0,0 +1,44 @@
+//Arrays
+let fruit=[1,2,"Apple","banana"];
+
+
+//push-add element to the end of the array
+// fruit.push("mongo");
+// console.log(fruit);
+
+//pop-remove an element from the last
+
+// console.log(fruit.pop());
+// console.log(fruit);
+
+//unshift()-to add element to the start of the aaray
+
+// fruit.unshift(234);
+// console.log(fruit);
+
+//shift-used to remove element from begining of the array
+
+// console.log(fruit.shift());
+// console.log(fruit);
+
+//indexOf-to compute index of the value in an array
+
+// console.log(fruit.indexOf('Apple'));
+
+//includes()-check if a value is included in an array or not
+
+// console.log(fruit.includes(45));
+
+//slice(start,ending)-return part of the array
+// console.log(fruit.slice(1,4))
+
+let num=[1,2,3,4,5,6];
+
+for(let i=0;i<num.length;i++){
+    num[i]+=2;
+}
+
+console.log(num);
+
+//Try implementing push,pop, shift,unshift,slice
+
