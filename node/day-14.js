2ed266eca14505ddda900d3872692f63b5bd01f7
[200~anshulojha1214@gmail.com <anshulojha1214@gmail.com>
2025-10-08 18:43:49 +0530

    files

--- node/day-14.js
+++ node/day-14.js
@@ -1,6 +1,19 @@
 const math=require("./math");
 
-console.log(math.add(2,4));
-console.log(math.mul(4,5));
-console.log(math.check(4));
+// console.log(math.add(2,4));
+// console.log(math.mul(4,5));
+// console.log(math.check(4));
+
+const fs=require('fs');
+
+// writing inside a file.
+
+// fs.writeFileSync('test.txt','replace the text');
+
+fs.writeFile('test.txt',"I am writing inside the file",err=>{
+    if(err) throw err;
+    console.log("File written");
+})
+
+
 
