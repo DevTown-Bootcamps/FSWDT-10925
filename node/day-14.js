8690fd5b5859ec3133e43c2c9c0e7388c450a4a0
[200~anshulojha1214@gmail.com <anshulojha1214@gmail.com>
2025-10-09 18:05:10 +0530

    fs

--- node/day-14.js
+++ node/day-14.js
@@ -10,10 +10,21 @@ const fs=require('fs');
 
 // fs.writeFileSync('test.txt','replace the text');
 
-fs.writeFile('test.txt',"I am writing inside the file",err=>{
-    if(err) throw err;
-    console.log("File written");
-})
+// fs.writeFile('test.txt',"I am writing inside the file",err=>{
+//     if(err) throw err;
+//     console.log("File written");
+// })
+
+//Append to a file
+// fs.appendFile('test.txt',"\n Append the new text",err=>{
+//     if(err) throwerr;
+//     console.log("New text has been added");
+// })
+
+//Read operation in files
+
+const data=fs.readFileSync('test.txt','utf-8');
+console.log(data);
 
 
 
